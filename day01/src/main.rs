04a07e134ae2d8269b4bbee135f2859bf0d7a945
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-12-03 17:51:46 +0100

    sikke noget rod

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::read_to_string;
+
 fn main() {
+    let input =
+        read_to_string(r"C:\Users\ander\Documents\GitHub\AoC\AoC\day01\src\input.txt").unwrap();
+    let split: Vec<String> = input.split("\n").map(|str| str.into()).collect();
+    let cleaned: i32 = split
+        .into_iter()
+        .map(|line| {
+            let chars: Vec<char> =line.chars()
+                .filter(|x| x.is_ascii_digit()).collect();
+                // dbg!(&chars);
+                format!("{}{}",chars.first().unwrap(),chars.last().unwrap())
+                // .fold("".into(), |acc, x| format!("{acc}{x}"))
+        })
+        // .inspect(|item| {dbg!(item);})
+        .map(|clean| clean.parse::<i32>().unwrap())
+        .sum();
+    dbg!(cleaned);
+    // let cleaned: Vec<String> = split.into_iter().map(|line|"".into()).collect();
     println!("Hello, world!");
 }
