ee052c4a16ecd781b4615dab0f5355b0451b97c8
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-12-04 15:08:25 +0100

    jep

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -10,11 +10,29 @@ fn main() {
 
     let cleaned = input
         .into_iter()
-        .map(|line| line.split_at(2+line.find(":").unwrap()).1.into())
+        .map(|line| line.split_at(2 + line.find(":").unwrap()).1.into())
         .collect::<Vec<String>>();
     // dbg!(cleaned);
+    let left: Vec<String> = cleaned
+        .iter()
+        .map(|line| line.split_at(line.find("|").unwrap()).0.into())
+        .collect();
+    let right: Vec<String> = cleaned
+        .iter()
+        .map(|line| line.split_at(1 + line.find("|").unwrap()).1.into())
+        .collect();
+    let winning_nums: Vec<Vec<i32>> = left.into_iter().map(|line| {
+        line.split_whitespace().map(|element| element.trim().parse::<i32>().unwrap()).collect::<Vec<i32>>()
+    }).collect();
+    let player_nums: Vec<Vec<i32>> = right.into_iter().map(|line| {
+        line.split_whitespace().map(|element| element.trim().parse::<i32>().unwrap()).collect::<Vec<i32>>()
+    }).collect();
+    // dbg!((winning_nums.len(),player_nums.len()));
+    let zipped: Vec<_> = winning_nums.into_iter().zip(player_nums.into_iter()).collect();
+
 }
 
-fn calc_score(left: &[i32],right: &[i32]) -> i32 {
-    todo!()
+fn calc_score(left: &[i32], right: &[i32]) -> i32 {
+    let mut amount: i32;
+    right.into_iter().for_each(|elem|left.)
 }
