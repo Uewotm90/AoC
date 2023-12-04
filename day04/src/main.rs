58dd96a9c85aa9a3abdc85c8be8b1014a7da90f7
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-12-04 16:03:31 +0100

    virker ikke

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -21,18 +21,46 @@ fn main() {
         .iter()
         .map(|line| line.split_at(1 + line.find("|").unwrap()).1.into())
         .collect();
-    let winning_nums: Vec<Vec<i32>> = left.into_iter().map(|line| {
-        line.split_whitespace().map(|element| element.trim().parse::<i32>().unwrap()).collect::<Vec<i32>>()
-    }).collect();
-    let player_nums: Vec<Vec<i32>> = right.into_iter().map(|line| {
-        line.split_whitespace().map(|element| element.trim().parse::<i32>().unwrap()).collect::<Vec<i32>>()
-    }).collect();
+    let winning_nums: Vec<Vec<i32>> = left
+        .into_iter()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|element| element.trim().parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+    let player_nums: Vec<Vec<i32>> = right
+        .into_iter()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|element| element.trim().parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect();
     // dbg!((winning_nums.len(),player_nums.len()));
-    let zipped: Vec<_> = winning_nums.into_iter().zip(player_nums.into_iter()).collect();
-
+    let zipped: Vec<_> = winning_nums
+        .into_iter()
+        .zip(player_nums.into_iter())
+        .collect();
+    let res: i32 = zipped
+        .iter()
+        .map(|pair| calc_score(pair.0.as_slice(), pair.1.as_slice()))
+        // .inspect(|elem| {dbg!(elem);})
+        .sum();
+    dbg!(res);
 }
 
 fn calc_score(left: &[i32], right: &[i32]) -> i32 {
-    let mut amount: i32;
-    // right.into_iter().for_each(|elem|left.)
+    let mut amount: u32 = 0;
+    right
+        .into_iter()
+        .for_each(|elem| match left.binary_search(elem) {
+            Ok(_) => amount = amount + 1,
+            Err(_) => (),
+        });
+        match amount {
+            0 => 0,
+            _ => 2_i32.pow(amount - 1)
+        }
+    
 }
