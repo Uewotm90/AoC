b9d9e1fcf56437ac7e00f2392151dbb9a2ed73ec
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-12-13 18:04:47 +0100

    playground time

---
+++ day09/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+}
+fn sequence_difference(sequence: &Vec<i32>) -> Vec<i32> {
+    let diff_seq = sequence
+        .iter()
+        .rev().pe
+    todo!()
+}
